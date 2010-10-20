606c04446a0620e6d75f95734ba5877c76b03ee9
Noah Tye <noah@tyes.us>
2010-10-20 13:19:43 -0400

    First commit. (includes bugs.)

---
+++ graffle.js
@@ -0,0 +1,67 @@
+var c; // canvas
+var ns = new Array(0);
+
+// Utilities /////////////////////////////////////////////////////////////////
+
+function map(a, f) {
+  for (var i = 0; i < a.length; i++)
+    a[i] = f(a[i]);
+  return a;
+}
+
+function draw_circle(x, y, r, borderstyle, fillstyle) {
+    c.beginPath();
+    // syntax reminder: x, y, r, start_angle, end_angle, anticlockwise
+    c.arc(x, y, r, 0, Math.PI*2, false);
+    c.closePath();
+    c.fillStyle = fillstyle;
+    c.strokeStyle = borderstyle;
+    c.lineWidth = 2;
+    c.stroke();
+    c.fill();
+}
+
+function d1(s) {
+    $('#d1').text(s);
+}
+
+function d2(s) {
+    $('#d2').text(s);
+}
+
+// Print debugging information.
+function d(n, s) {
+    // Make sure the list is big enough.
+    while ($('#debuglist').children().length < n) {
+	$('#debuglist').children().last().after('<li>.</li>');
+    }
+    $('#debuglist').children().last().text(s);
+}
+
+
+
+function draw_node(x, y) {
+    var r = 25;
+    draw_circle(x, y, r, '#222', '#FCF0AD');
+
+    ns[ns.length] = {'x': x, 'y': y};
+    ns[ns.length-1].covers = function(x, y) {
+	return (this.x - x)^2 + (this.y - y)^2 < r^2;
+    };
+}
+
+
+$(document).ready(function() {
+	c = $('#main').get(0).getContext('2d');
+	$('#main').click(function(e) {
+		var canvasX = e.clientX - $(this).position().left;
+		var canvasY = e.clientY - $(this).position().top;
+		map(ns, function(n) {
+			d1(n.x);
+			d2(canvasX);
+			return n.covers(canvasX, canvasY);
+		    });
+		draw_node(canvasX, canvasY);
+		return true;
+	    });
+    });

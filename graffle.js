42d3e571ecdafd6263ab3e83b0729f1b64833ccf
Noah Tye <noah@tyes.us>
2010-10-31 14:50:56 -0400

    graffleEval now returns graffle leaves. had to restructure node creation/placement.

--- graffle.js
+++ graffle.js
@@ -1,6 +1,3 @@
-var c; // canvas
-var ns = new Array(0);
-
 // Utilities /////////////////////////////////////////////////////////////////
 
 function square(x) {
@@ -11,26 +8,26 @@ function strcoords(x, y) {
     return '(' + x + ', ' + y + ')';
 }
 
-function draw_circle(x, y, r, borderstyle, fillstyle) {
-    c.beginPath();
+function drawCircle(context, x, y, r, borderstyle, fillstyle) {
+    context.beginPath();
     // syntax reminder: x, y, r, start_angle, end_angle, anticlockwise
-    c.arc(x, y, r, 0, Math.PI*2, false);
-    c.closePath();
-    c.fillStyle = fillstyle;
-    c.strokeStyle = borderstyle;
-    c.lineWidth = 2;
-    c.stroke();
-    c.fill();
+    context.arc(x, y, r, 0, Math.PI*2, false);
+    context.closePath();
+    context.fillStyle = fillstyle;
+    context.strokeStyle = borderstyle;
+    context.lineWidth = 2;
+    context.stroke();
+    context.fill();
 }
 
-function draw_text(x, y, s) {
-    c.font = '12px sans-serif';
-    c.textAlign = 'center';
-    c.fillStyle = '#222';
-    c.fillText(s, x, y);
+function drawText(context, x, y, s) {
+    context.font = '12px sans-serif';
+    context.textAlign = 'center';
+    context.fillStyle = '#222';
+    context.fillText(s, x, y);
 }
 
-function draw_line(x1, y1, x2, y2) {
+function draw_line(c, x1, y1, x2, y2) {
     c.beginPath();
     c.moveTo(x1, y1);
     c.lineTo(x2, y2);
@@ -63,9 +60,72 @@ function find_node(name) {
     }
 }
 
-function make_node(x, y, name) {
-    n =  {'x': x, 'y': y, 'r': NODE_R,
-	  'connections': new Array(0), 'name': name};
+function makeSpace(name, canvasElement) {
+    var space = {'name': name,
+		 'context': canvasElement.getContext('2d'),
+		 'nodes': new Array(0)};
+
+    $(canvasElement)
+      .mousedown(function(e) {
+	      e.preventDefault();
+	      var canvasX = e.clientX - $(this).position().left;
+	      var canvasY = e.clientY - $(this).position().top;
+	      if (e.which == 1) { // left click
+		  space.nodes.forEach(function(n) {
+			  // if the user mousedowned on a node...
+			  if (n.covers(canvasX, canvasY)) {
+			      n.drawActive();
+			      activeNode = n;
+			  }
+		      });
+	      } else if (e.which == 3) { // right click
+		  space.nodes.forEach(function(n) {
+			  if (n.covers(canvasX, canvasY)) {
+			      n.drawActive();
+			      n.toChildren(function(child) {
+				      child.drawActive();
+				  });
+			  }
+		      });
+	      }
+	  })
+      .mouseup(function(e) {
+	      e.preventDefault();
+	      var canvasX = e.clientX - $(this).position().left;
+	      var canvasY = e.clientY - $(this).position().top;
+	      if (e.which == 1) { // left click
+		  if (activeNode) {
+		      space.nodes.forEach(function(n) {
+			      if (n.covers(canvasX, canvasY)) {
+				  connectNodes(activeNode, n);
+			      }
+			  });
+		      activeNode.drawDefault();
+		      activeNode = false;
+		  } else {
+		      if (space.nodes.every(function(n) {
+				  return !n.overlaps(canvasX, canvasY,
+						     NODE_R);
+			      })) {
+			  n = makeNode('');
+			  placeNode(canvasX, canvasY, n, space);
+		      };
+		  }
+	      } else if (e.which == 3) { // right click
+		  space.nodes.forEach(function(n) {
+			  n.drawDefault();
+			  if (n.covers(canvasX, canvasY)) {
+			      placeNode(50, 50, graffleEval(n), resultspace);
+			  }
+		      });
+	      }
+	  });
+
+    return space;
+}
+
+function makeNode(name) {
+    n = { 'name': name, 'connections': new Array(0) };
 
     // graph methods
     n.connectTo = function(n) {
@@ -87,130 +147,95 @@ function make_node(x, y, name) {
 	    });
     }
 
+    // Allowing empty name strings then setting them interactively is a hack.
+    if (n.name == '') {
+	n.name = prompt("This node's name:");
+    }
+    return n;
+}
+
+function placeNode(x, y, node, space) {
+    space.nodes.push(node);
+    node.space = space;
+    node.x = x;
+    node.y = y;
+    node.r = NODE_R;
     // geometry methods
-    n.overlaps = function(x, y, r) {
+    node.overlaps = function(x, y, r) {
 	return square(this.x - x) + square(this.y - y) < square(this.r + r);
     };
-    n.covers = function(x, y) {
+    node.covers = function(x, y) {
 	return square(this.x - x) + square(this.y - y) < square(this.r);
     };
-    n.strcoords = function() { // for debugging
+    node.strcoords = function() { // for debugging
 	return strcoords(this.x, this.y);
     };
 
     // drawing methods
-    n.draw = function(bgcolor) {
-	draw_circle(this.x, this.y, this.r, '#222', bgcolor);
-	draw_text(this.x, this.y, this.name);
+    node.draw = function(bgcolor) {
+	drawCircle(this.space.context, this.x, this.y, this.r, '#222', bgcolor);
+	drawText(this.space.context, this.x, this.y, this.name);
     };
-    n.drawDefault = function() { this.draw('#FCF0AD'); }
-    n.drawActive = function() { this.draw('#669'); }
-    n.drawDefault();
-    // Allowing empty name strings then setting them interactively is a hack.
-    if (n.name == '') {
-	n.name = prompt("This node's name:");
-	n.drawDefault();
-    }
-    ns.push(n);
+    node.drawDefault = function() { this.draw('#FCF0AD'); };
+    node.drawActive = function() { this.draw('#669'); };
+    node.drawAsResult = function() {
+	drawCircle(this.space.context, 50, 50, this.r, '#222', '#FCF0AD');
+    };
+    
+    node.drawDefault();
 }
 
 function connectNodes(a, b) {
-    draw_line(a.x, a.y, b.x, b.y);
-    a.drawDefault();
-    b.drawDefault();
-    a.connectTo(b);
-    b.connectTo(a);
+    if (a.space == b.space) {
+	draw_line(a.space.context, a.x, a.y, b.x, b.y);
+	a.drawDefault();
+	b.drawDefault();
+	a.connectTo(b);
+	b.connectTo(a);
+    } else {
+	alert("Did you just try to connect two nodes in different spaces?  It won't work, you know.");
+    }
 }
-
 
 function graffleEval(n) {
     // Special forms first.
     if (n.name == 'leaf?') {
 	if (n.children().length == 1) {
 	    if (n.children()[0].children().length == 0) {
-		return 't';
+		return makeNode('t');
 	    } else {
-		return 'f';
+		return makeNode('f');
 	    }
 	} else {
-	    alert('Error: atom? expected one argument, got ' +
+	    alert('Error: leaf? expected one argument, got ' +
 		  n.children().length);
 	    return;
 	}
     }
-    // Regular functions.
+    // Regular functions: first eval all children:
     children = n.children().map(graffleEval);
     if (n.name == '+') {
-	return children.reduce(function(a,b) { return a + b; });
+	return makeNode(children.reduce(function(a, b) {
+		    return a.name + b.name; }));
     } else if (n.name == '-') {
-	return children.reduce(function(a, b) { return a - b; });
+	return makeNode(children.reduce(function(a, b) {
+		    return a.name - b.name; }));
     } else if (n.name == '*') {
-	return children.reduce(function(a, b) { return a * b; });
+	return makeNode(children.reduce(function(a, b) {
+		    return a.name * b.name; }));
     } else if (n.name == '/') {
-	return children.reduce(function(a, b) { return a / b; });
+	return makeNode(children.reduce(function(a, b) {
+		    return a.name / b.name; }));
     } else if (!isNaN(parseFloat(n.name))) {
-	return parseFloat(n.name);
+	return makeNode(parseFloat(n.name));
     } else {
 	alert(n.name + ' is not callable.');
     }
 }
-
+var mainspace;
+var resultspace;
 $(document).ready(function() {
-  $('#main').bind('contextmenu', function(e) { return false; });
-
-  c = $('#main').get(0).getContext('2d');
-
-  $('#main')
-      .mousedown(function(e) {
-	      e.preventDefault();
-	      var canvasX = e.clientX - $(this).position().left;
-	      var canvasY = e.clientY - $(this).position().top;
-	      if (e.which == 1) { // left click
-		  ns.forEach(function(n) {
-			  // if the user mousedowned on a node...
-			  if (n.covers(canvasX, canvasY)) {
-			      n.drawActive();
-			      activeNode = n;
-			  }
-		      });
-	      } else if (e.which == 3) { // right click
-		  ns.forEach(function(n) {
-			  if (n.covers(canvasX, canvasY)) {
-			      n.drawActive();
-			      n.toChildren(function(child) {
-				      child.drawActive();
-				  });
-			  }
-		      });
-	      }
-	  })
-      .mouseup(function(e) {
-	      e.preventDefault();
-	      var canvasX = e.clientX - $(this).position().left;
-	      var canvasY = e.clientY - $(this).position().top;
-	      if (e.which == 1) { // left click
-		  if (activeNode) {
-		      ns.forEach(function(n) {
-			      if (n.covers(canvasX, canvasY)) {
-				  connectNodes(activeNode, n);
-			      }});
-		      activeNode.drawDefault();
-		      activeNode = false;
-		  } else {
-		      if (ns.every(function(n) {
-				  return !n.overlaps(canvasX, canvasY,
-						     NODE_R);
-			      })) {
-			  make_node(canvasX, canvasY, '');
-		      };
-		  }
-	      } else if (e.which == 3) { // right click
-		  ns.forEach(function(n) {
-			  n.drawDefault();
-			  if (n.covers(canvasX, canvasY)) {
-			      d(1, 'Result: ' + graffleEval(n));
-			  }
-		      });
-	      }
-	  });
+	$('#main').bind('contextmenu', function(e) { return false; });
+	mainspace = makeSpace('main', $('#main').get(0));
+	resultspace = makeSpace('result', $('#result').get(0));
     });
